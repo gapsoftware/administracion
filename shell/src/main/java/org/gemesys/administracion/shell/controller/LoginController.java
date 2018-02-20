01f9f09de529e8ed7f3bad46813880af68f095ee
gabriel.perezv <gabriel.perezv@uchile.cl>
2018-02-19 23:04:50 -0300

    pruebas con Spring Security

--- shell/src/main/java/org/gemesys/administracion/shell/controller/LoginController.java
+++ shell/src/main/java/org/gemesys/administracion/shell/controller/LoginController.java
@@ -31,17 +31,26 @@ public class LoginController {
         return modelAndView;
     }
 
+    @RequestMapping(value="/home", method = RequestMethod.GET)
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("/home");
+        return modelAndView;
+    }
 
-    @RequestMapping(value="/admin/registration", method = RequestMethod.GET)
+
+    @RequestMapping(value="/registration", method = RequestMethod.GET)
     public ModelAndView registration(){
         ModelAndView modelAndView = new ModelAndView();
         User user = new User();
         modelAndView.addObject("user", user);
-        modelAndView.setViewName("/admin/registration");
+        modelAndView.setViewName("/registration");
         return modelAndView;
     }
 
-    @RequestMapping(value = "/admin/registration", method = RequestMethod.POST)
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
     public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
         ModelAndView modelAndView = new ModelAndView();
         User userExists = userService.findUserByEmail(user.getEmail());
@@ -51,27 +60,43 @@ public class LoginController {
                             "There is already a user registered with the email provided");
         }
         if (bindingResult.hasErrors()) {
-            modelAndView.setViewName("/admin/registration");
+            modelAndView.setViewName("/registration");
         } else {
             userService.saveUser(user);
             modelAndView.addObject("successMessage", "User has been registered successfully");
             modelAndView.addObject("user", new User());
-            modelAndView.setViewName("/admin/registration");
+            modelAndView.setViewName("/registration");
 
         }
         return modelAndView;
     }
 
-    @RequestMapping(value="/admin/home", method = RequestMethod.GET)
-    public ModelAndView home(){
+    @RequestMapping(value="/admin/main", method = RequestMethod.GET)
+    public ModelAndView admin_main(){
         ModelAndView modelAndView = new ModelAndView();
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = userService.findUserByEmail(auth.getName());
         modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName1() + " (" + user.getEmail() + ")");
         modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
-        modelAndView.setViewName("admin/home");
+        modelAndView.setViewName("admin/main");
         return modelAndView;
     }
 
+    @RequestMapping(value="/account/main", method = RequestMethod.GET)
+    public ModelAndView account_main(){
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName1() + " (" + user.getEmail() + ")");
+        modelAndView.addObject("accountMessage","Content Available Only for Users with Account Role");
+        modelAndView.setViewName("account/main");
+        return modelAndView;
+    }
 
+    @RequestMapping(value="/access-denied", method = RequestMethod.GET)
+    public ModelAndView noaccess(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/access-denied");
+        return modelAndView;
+    }
 }
