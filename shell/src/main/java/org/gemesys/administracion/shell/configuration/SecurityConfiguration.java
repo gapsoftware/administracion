ed283c3feb82ef42845a5f2dfcdf1b664ee3852f
Gabriel Pérez V <gabriel.perezv@uchile.cl>
2018-06-27 18:42:31 -0400

    desarrollo-tabla--admin-usuarios-parte4-configurando-set-resultados

--- shell/src/main/java/org/gemesys/administracion/shell/configuration/SecurityConfiguration.java
+++ shell/src/main/java/org/gemesys/administracion/shell/configuration/SecurityConfiguration.java
@@ -73,6 +73,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web
                 .ignoring()
-                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "/rest/**");
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
     }
 }
--- shell/src/main/java/org/gemesys/administracion/shell/repository/UserRepository.java
+++ shell/src/main/java/org/gemesys/administracion/shell/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package org.gemesys.administracion.shell.repository;
 
 import org.gemesys.administracion.shell.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository("userRepository")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+    Page<User> findAll(Pageable pageable);
+
     User findByEmail(String email);
 }
