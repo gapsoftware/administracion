95a018e815babe9f7aade198e16c4d1a53750ea0
Gabriel Pérez V <gabriel.perezv@uchile.cl>
2018-06-28 17:26:24 -0400

    desarrollo-tabla--admin-usuarios-parte5-configurando-search-y-sort

--- shell/src/main/java/org/gemesys/administracion/shell/repository/UserRepository.java
+++ shell/src/main/java/org/gemesys/administracion/shell/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.gemesys.administracion.shell.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -16,4 +17,7 @@ public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     Page<User> findAll(Pageable pageable);
 
     User findByEmail(String email);
+
+    Page<User> findByEmailContains(Pageable pageable, @Param("email") String email);
+
 }
